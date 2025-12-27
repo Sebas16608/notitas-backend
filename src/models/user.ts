186cb4fcd8e9f31b86a428e2349893f21cd247d3
Sebas16608 <asebasrr444@gmail.com>
2025-12-26 22:17:14 -0600

    Creando los modelos de user

---
+++ src/models/user.ts
@@ -0,0 +1,60 @@
+import { DataTypes, Model } from "sequelize";
+import sequelize from "../config/database";
+import bcrypt from "bcrypt";
+
+class User extends Model {
+    declare id?: number;
+    declare name: string;
+    declare email: string;
+    declare password: string;
+};
+
+User.init({
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true,
+    },
+    name: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+        validate: {
+            len: [1, 255]
+        }
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        validate: {
+            isEmail: true
+        }
+    },
+    password: {
+        type: DataTypes.STRING(10),
+        allowNull: false,
+        validate: {
+            len: [1, 10]
+        }
+    },
+},
+{
+    sequelize,
+    modelName: "User",
+    tableName: "User",
+
+    hooks: {
+        beforeCreate: async (user: User) => {
+            const salt = await bcrypt.genSalt(10)
+            user.password = await bcrypt.hash(user.password, salt);
+        },
+        beforeUpdate: async (user: User) => {
+            if (user.changed("password")) {
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(user.password, salt);
+            }
+        },
+    },
+}
+);
+
+export default User;

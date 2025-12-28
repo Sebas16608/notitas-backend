86dffc9d4e522ef0ad09b3a744ba7a18487831a3
Sebas16608 <asebasrr444@gmail.com>
2025-12-27 19:40:19 -0600

    defniendo los metodos GET de la API de user

---
+++ src/controllers/user-controller.ts
@@ -0,0 +1,24 @@
+import User from "../models/users";
+import { Request, Response } from "express";
+
+export const getAllUsers = async (req: Request, res: Response) => {
+    try{
+        const user = await User.findAll();
+        return res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: "Internal Server Error"});
+    };
+};
+
+export const getUserById = async (req: Request, res: Response) => {
+    try {
+        const user = await User.findByPk(req.params.id);
+        if (!user) return res.status(404).json({ error: "Not Found"});
+
+        return res.status(200).json(user);
+    } catch (err) {
+        console.log(err);
+        return res.status(500).json({ error: "Internal Server Error" });
+    }
+}

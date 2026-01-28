21ce5204f1dadfa825346474229563b375565019
Sebas16608 <asebasrr444@gmail.com>
2026-01-27 20:52:48 -0600

    fix: Fixing the password field

--- Notitas/Models/User.cs
+++ Notitas/Models/User.cs
@@ -5,7 +5,7 @@ namespace Users.Model
         public int Id { get; set; }
         public required string Name { get; set; }
         public required string Email { get; set; }
-        public required string Password { get; set; }
+        public required string PasswordHash { get; set; }
 
     }
 }

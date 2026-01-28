21318d650ca799004432898b704c257ecbfdee66
Sebas16608 <asebasrr444@gmail.com>
2026-01-27 22:00:31 -0600

    chore: Making nullable the field id

--- Notitas/Models/User.cs
+++ Notitas/Models/User.cs
@@ -2,7 +2,7 @@ namespace Users.Model
 {
     public class User
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
         public required string Name { get; set; }
         public required string Email { get; set; }
         public required string PasswordHash { get; set; }

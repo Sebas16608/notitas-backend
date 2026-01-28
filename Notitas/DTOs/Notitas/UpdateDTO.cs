7d8fb490750f6e5b07b0a786a308e8a791ffed52
Sebas16608 <asebasrr444@gmail.com>
2026-01-28 06:21:24 -0600

    chore: Creating the update dto by notitasc

---
+++ Notitas/DTOs/Notitas/UpdateDTO.cs
@@ -0,0 +1,9 @@
+namespace Notitas.DTO
+{
+    public class NotitaUpdate
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+    }
+}

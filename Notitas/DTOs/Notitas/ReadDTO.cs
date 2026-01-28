eab45396588566eb53cc66d915f759046cf17877
Sebas16608 <asebasrr444@gmail.com>
2026-01-27 22:12:34 -0600

    chore: Creating the read dto by notitasc

---
+++ Notitas/DTOs/Notitas/ReadDTO.cs
@@ -0,0 +1,9 @@
+namespace Notitas.DTO
+{
+    public class NotitaRead
+    {
+        public required string Title { get; set; }
+        public required string Content { get; set; }
+        public required string Name { get; set; }
+    }
+}

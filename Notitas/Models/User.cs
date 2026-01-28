41d5180f02b4102952a28f669cdaa931aa2a95b7
Sebas16608 <asebasrr444@gmail.com>
2026-01-27 18:49:47 -0600

    chore: Creating the User models

---
+++ Notitas/Models/User.cs
@@ -0,0 +1,11 @@
+namespace Users.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
+
+    }
+}
